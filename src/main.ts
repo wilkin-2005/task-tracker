2fc5933baf95227b2dbab504bebb8193ee3e5108
Wilmer Kindstedt <wilmer.kindstedt@gmail.com>
2026-06-17 11:36:42 +0200

    Gjorde övningen för dag 3: - La alla task:s namn i en array som sedan skrivs ut i konsolen med hjälp av en loop. - Lade också till en if-sats med ett meddelande som skrivs ut i konsolen om det inte fanns några tasks i arrayen.

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,15 @@
-
 // Kompilera en gång: "npx tsc"
 // Automatisk kompilering: "npm run watch"
 
-// TASKS
-const taskName1: string = "Lära mig Typescript";
-const priority1: number = 5;
-const isCompleted1: boolean = false;
-
-const taskName2: string = "Vattna blommorna";
-const priority2: number = 2;
-const isCompleted2: boolean = true;
-
-const taskName3: string = "Fira midsommar";
-const priority3: number = 3;
-const isCompleted3: boolean = false;
-
-// Andel avklarade
-const completedTasks: number = 1;
-const totalTasks: number = 3;
-const comletionRate:number = completedTasks / totalTasks * 100;
+const tasks = [
+    "Lära mig Typescript",
+    "Vattna blommorna",
+    "Fira midsommar",
+    "Träna",
+    "Handla",
+    "Tvätta",
+    "Plugga"
+];
 
 console.log(`
 ///==============================\\\\\\
@@ -26,28 +17,27 @@ console.log(`
 \\\\\\==============================///
  `);
 
-console.log(`
-TASK 1
-Task: ${taskName1}
-Priority: ${priority1}
-Completed: ${isCompleted1}
- `);
 
-console.log(`
-TASK 2
-Task: ${taskName2}
-Priority: ${priority2}
-Completed: ${isCompleted2}
- `);
+if (tasks.length > 0){
 
-console.log(`
-TASK 3
-Task: ${taskName3}
-Priority: ${priority3}
-Completed: ${isCompleted3}
- `);
+    for (let i = 0; i < tasks.length; i++) {
+        console.log(`Uppgift ${i+1}: ${tasks[i]}`);
+    }
 
-console.log(`
-Completion rate:
-${completedTasks}/${totalTasks}
-${comletionRate.toFixed(2)}%`);
+    console.log(`\nAntal uppgifter: ${tasks.length}`);
+}
+else {
+    console.log(`Alla uppgifter är avklarade!`);
+}
+
+/*
+    // Andel avklarade
+    const completedTasks: number = 1;
+    const totalTasks: number = 3;
+    const comletionRate:number = completedTasks / totalTasks * 100;
+
+    console.log(`
+    Completion rate:
+    ${completedTasks}/${totalTasks}
+    ${comletionRate.toFixed(2)}%`);
+*/
