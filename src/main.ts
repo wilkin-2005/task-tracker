f0b4b8d90481a7e1372c3ccb669d84739b4a15ff
Wilmer Kindstedt <wilmer.kindstedt@gmail.com>
2026-06-18 12:04:44 +0200

    • Fixade en sak med writeAll() • Fixade med utseendet så att det ser snyggare ut när det skrivs ut i konsolen.

--- src/main.ts
+++ src/main.ts
@@ -23,9 +23,17 @@ function writeHeader() {
 // Skapa en funktion som skriver ut alla våra tasks i en lista i konsolen.
 function writeTaskList() {
     if (tasks.length > 0){
+        console.log("\t[Uppgifter]");
+
         for (let i = 0; i < tasks.length; i++) {
-            console.log(`Uppgift ${i+1}: ${tasks[i]}`);
+            console.log(`${i+1}) ${tasks[i]}`);
         }
+
+        /* for ... of som skriver ut i punktlista istället för numrerat.
+        for (const task of tasks) {
+            console.log(`• ${task}`);
+        }
+        */
     }
     else {
         console.log("!! Inga aktiva uppgifter !!");
@@ -34,11 +42,14 @@ function writeTaskList() {
 
 // Skapa en funktion som skriver ut statistiken för hur många tasks vi har i vår lista:
 function writeStatistics() {
-    console.log(`\n Antal uppgifter: ${tasks.length}
-====================`);
+    console.log(`\nAntal uppgifter: ${tasks.length}`);
 }
 
-function writeAll() {
+// Skriver ut alla delar av sidan. Tömmer konsolen innan om så önskas.
+function writeAll(doClear: boolean = false) {
+    if (doClear)
+        console.clear();
+
     writeHeader();
     writeTaskList();
     writeStatistics();
@@ -47,8 +58,8 @@ function writeAll() {
 // Skapa en funktion som lägger till en ny uppgift i vår lista.
 function addTask(newTask: string) {
     tasks.push(newTask);
+    console.log(`Lade till ny uppgift: "${newTask}" \n `);
 }
-
 
 // Kör funktionerna i följande ordning i programmet för att se att det stämmer:
 writeAll();
