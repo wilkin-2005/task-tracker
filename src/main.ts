f13a3e34476a7eca59a33f33550ae09fad0688de
Wilmer Kindstedt <wilmer.kindstedt@gmail.com>
2026-06-18 11:27:49 +0200

    Gjorde Typescript-övningen för dag 4.

--- src/main.ts
+++ src/main.ts
@@ -11,33 +11,46 @@ const tasks: string[] = [
     "Plugga"
 ];
 
-console.log(`
-///==============================\\\\\\
-|||         TASK TRACKER         |||
-\\\\\\==============================///
- `);
-
-
-if (tasks.length > 0){
-
-    for (let i = 0; i < tasks.length; i++) {
-        console.log(`Uppgift ${i+1}: ${tasks[i]}`);
-    }
-
-    console.log(`\nAntal uppgifter: ${tasks.length}`);
-}
-else {
-    console.log(`Alla uppgifter är avklarade!`);
-}
-
-/*
-    // Andel avklarade
-    const completedTasks: number = 1;
-    const totalTasks: number = 3;
-    const comletionRate:number = completedTasks / totalTasks * 100;
-
+// Skapa en funktion som skriver ut vår "header" i konsolen.
+function writeHeader() {
     console.log(`
-    Completion rate:
-    ${completedTasks}/${totalTasks}
-    ${comletionRate.toFixed(2)}%`);
-*/
+    ///==============================\\\\\\
+    |||         TASK TRACKER         |||
+    \\\\\\==============================///
+    `);
+}
+
+// Skapa en funktion som skriver ut alla våra tasks i en lista i konsolen.
+function writeTaskList() {
+    if (tasks.length > 0){
+
+        for (let i = 0; i < tasks.length; i++) {
+            console.log(`Uppgift ${i+1}: ${tasks[i]}`);
+        }
+    }
+    else {
+        console.log(`Alla uppgifter är avklarade!`);
+    }
+}
+
+// Skapa en funktion som skriver ut statistiken för hur många tasks vi har i vår lista:
+function writeStatistics() {
+    console.log(`\n Antal uppgifter: ${tasks.length}
+====================`);
+}
+
+// Skapa en funktion som lägger till en ny uppgift i vår lista.
+function addTask(newTask: string) {
+    tasks.push(newTask);
+}
+
+
+// Kör funktionerna i följande ordning i programmet för att se att det stämmer:
+writeHeader();
+writeTaskList();
+writeStatistics();
+
+addTask("Tömma och fylla diskmaskinen");
+
+writeTaskList();
+writeStatistics();
