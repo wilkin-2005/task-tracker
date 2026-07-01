7c16259e23e9617b5171b94d5b13744cfd53182a
Wilmer Kindstedt <wilmer.kindstedt@gmail.com>
2026-07-01 14:36:30 +0200

    Skapade en form.ts enligt bonusuppgift 1 för dag 11 av Typescript och fixade alla imports och export som den ändringen krävde.

---
+++ src/form.ts
@@ -0,0 +1,73 @@
+
+import type { PriorityLevels } from "./types.js";
+import { addTask, tasks } from "./tasks.js";
+
+// Variabler
+const nameInput = document.querySelector("#task-name") as HTMLInputElement;
+const priorityInput = document.querySelector("#priority-input") as HTMLSelectElement;
+const errorMessage = document.querySelector("#error-message") as HTMLParagraphElement;
+// const descriptionInput = document.querySelector("#description-input") as HTMLTextAreaElement;
+
+
+// Hanterar submits från formuläret för att lägga till nya uppgifter
+export function handleSubmit(event: SubmitEvent): void
+{
+    event.preventDefault();
+
+    const taskName: string = nameInput.value.trim();
+    const priority = priorityInput.value as PriorityLevels;
+    // const description: string = descriptionInput.value;
+
+    if (!validTask(taskName, priority) ) {
+        return;
+    }
+
+    clearForm();
+    addTask(taskName, priority);
+}
+
+
+// Kollar om en submitad ny task är godkänd
+function validTask(taskName: string, priority: PriorityLevels): boolean
+{
+    // Kontrollerar namn
+    if (taskName === "") {
+        errorMessage.textContent = "Ogiltigt namn";
+        return false;
+    }
+    else if (taskName.length < 3 || taskName.length > 32) {
+        errorMessage.textContent = "Namnet måste vara mellan 3 till 32 tecken långt";
+        return false;
+    }
+
+    // Kontrollerar namndubletter
+    let doNameExist: boolean = false;
+
+    tasks.forEach(task => {
+        if ( task.name.toUpperCase() === taskName.toUpperCase() ) {
+            doNameExist = true;
+        }
+    });
+
+    if (doNameExist) {
+        errorMessage.textContent = "Finns redan uppgift med detta namn";
+        return false;
+    }
+    
+    // Kontrollerar prioritet
+    if (priority !== "hög" && priority !== "medel" && priority !== "låg") {
+        errorMessage.textContent = "Du måste välja en prioritetsnivå";
+        return false;
+    }
+
+    return true;
+}
+
+
+// Rensar formuläret
+function clearForm(): void
+{
+    errorMessage.textContent = "";
+    nameInput.value = "";
+    priorityInput.selectedIndex = 0;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,87 +1,12 @@
 
 // Importerar från andra .ts-filer
-import type { PriorityLevels } from "./types.js";
 import { renderAllTasks } from "./render.js";
-import { addTask, tasks } from "./tasks.js";
+import { handleSubmit } from "./form.js";
 import { loadTasks } from "./storage.js";
-
-
 
 // DOM variabler
 const form = document.querySelector("#task-form") as HTMLFormElement;
 form.addEventListener("submit", handleSubmit);
-const nameInput = document.querySelector("#task-name") as HTMLInputElement;
-const priorityInput = document.querySelector("#priority-input") as HTMLSelectElement;
-// const descriptionInput = document.querySelector("#description-input") as HTMLTextAreaElement;
-
-const errorMessage = document.querySelector("#error-message") as HTMLParagraphElement;
-
-
-// Hanterar submits från formuläret för att lägga till nya uppgifter
-function handleSubmit(event: SubmitEvent): void
-{
-    event.preventDefault();
-
-    const taskName: string = nameInput.value.trim();
-    const priority = priorityInput.value as PriorityLevels;
-    // const description: string = descriptionInput.value;
-
-    if (!validTask(taskName, priority) ) {
-        return;
-    }
-
-    clearForm();
-    addTask(taskName, priority);
-}
-
-
-
-// Rensar formuläret
-function clearForm(): void
-{
-    errorMessage.textContent = "";
-    nameInput.value = "";
-    priorityInput.selectedIndex = 0;
-}
-
-
-
-// Kollar om en submitad ny task är godkänd
-function validTask(taskName: string, priority: PriorityLevels): boolean
-{
-    // Kontrollerar namn
-    if (taskName === "") {
-        errorMessage.textContent = "Ogiltigt namn";
-        return false;
-    }
-    else if (taskName.length < 3 || taskName.length > 32) {
-        errorMessage.textContent = "Namnet måste vara mellan 3 till 32 tecken långt";
-        return false;
-    }
-
-    // Kontrollerar namndubletter
-    let doNameExist: boolean = false;
-
-    tasks.forEach(task => {
-        if ( task.name.toUpperCase() === taskName.toUpperCase() ) {
-            doNameExist = true;
-        }
-    });
-
-    if (doNameExist) {
-        errorMessage.textContent = "Finns redan uppgift med detta namn";
-        return false;
-    }
-    
-    // Kontrollerar prioritet
-    if (priority !== "hög" && priority !== "medel" && priority !== "låg") {
-        errorMessage.textContent = "Du måste välja en prioritetsnivå";
-        return false;
-    }
-
-    return true;
-}
-
 
 loadTasks();
 renderAllTasks();
